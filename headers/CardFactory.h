25daf2533d793532f37fb0475d3059eba9194765
LeFlou <leflou@gmx.com>
2015-08-10 19:54:15 +0200

    Templated dynamic cache to store cards

--- headers/CardFactory.h
+++ headers/CardFactory.h
@@ -1,11 +1,26 @@
 #pragma once
 
-#include "CharacterCard.h"
+#include <map>
 
 namespace Citadel
 {
     namespace CardFactory
     {
-        const CharacterCard& GetCharacterCard(const Character character);
+        template <typename Key, typename Value>
+        const Value& GetCard(const Key key)
+        {
+            static std::map<Key, Value> cache;
+
+            auto it = cache.find(key);
+            if (it != cache.end())
+            {
+                return it->second;
+            }
+            else
+            {
+                cache.insert(std::make_pair(key, Value(key)));
+                return cache.at(key);
+            }
+        }
     }
 }
