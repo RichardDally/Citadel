04c48966e82f2c268c9bb36a19d3d1ab57d156fb
Richard Dally <r.dally@protonmail.com>
2016-10-28 19:40:54 +0200

    Catch standard exceptions directly in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
 #include "ConsoleMenu.h"
 
 int main()
 {
     // TODO: parse args
-    Citadel::ConsoleMenu::Start();
+    try
+    {
+        Citadel::ConsoleMenu::Start();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cerr << exception.what() << "\n";
+    }
+    
     return 0;
 }
